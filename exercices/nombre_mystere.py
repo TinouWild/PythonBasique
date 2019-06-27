ea15bd083a6d76f2c77f37a7c66e1bd5ad03d933
TinouWild <tinouclt@gmail.com>
2019-06-27 10:08:35 +0200

    nb mystere pt5

--- exercices/nombre_mystere.py
+++ exercices/nombre_mystere.py
@@ -1,17 +1,23 @@
 import random
 
-nb_mystere = random.randint(1, 1000)
-nb_utilisateur = input("Quel est le nombre mystère ?")
+nb_mystere = random.randint(1, 50)
+i = 1
+while i <= 5:
+    nb_utilisateur = input(f"Essai numéro {i} ::: Quel est le nombre mystère ?")
+    if nb_utilisateur.isdigit():
+        nb_utilisateur = int(nb_utilisateur)
+    else:
+        exit()
 
-if nb_utilisateur.isdigit():
-    nb_utilisateur = int(nb_utilisateur)
-else:
-    exit()
+    if nb_mystere < nb_utilisateur:
+        print(f"Le nombre mystère est plus petit que {nb_utilisateur}")
+    elif nb_mystere > nb_utilisateur:
+        print(f"Le nombre mystère est plus grand que {nb_utilisateur}")
+    else:
+        print("Bravo, vous avez trouvé le nombre mystère !")
+        exit()
+    i += 1
+print(f"Vous avez perdu, le nombre mystère était {nb_mystere}")
 
-if nb_mystere < nb_utilisateur:
-    print(f"Le nombre mystère est plus petit que {nb_utilisateur}")
-elif nb_mystere > nb_utilisateur:
-    print(f"Le nombre mystère est plus grand que {nb_utilisateur}")
-else:
-    print("Bravo, vous avez trouvé le nombre mystère !")
+
 
