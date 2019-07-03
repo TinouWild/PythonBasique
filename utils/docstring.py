8442bf1d5f791d9177c4dbd63e1398c9b1cad4ed
TinouWild <tinouclt@gmail.com>
2019-07-03 14:46:14 +0200

    docstring

---
+++ utils/docstring.py
@@ -0,0 +1,11 @@
+def function_example(nom, age):
+    """Description de la fonction
+
+    Args:
+        nom (str): nom d'utilisateur
+        age(int): âge de l'utilisateur
+
+    Returns:
+        list: liste de nombres
+    """
+    return [1, 2, 3]
