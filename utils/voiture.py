39f67ef1e97bc105da2fc1d9af3b714f34248b01
etienne <etienne@ubuntu-tinou.2le.net>
2019-07-22 13:57:58 +0200

    exercice voiture

--- utils/voiture.py
+++ utils/voiture.py
@@ -1,9 +1,26 @@
 class Voiture:
-    voitures_crees = 0
-
-    def __init__(self, marque):
-        Voiture.voitures_crees += 1
+    voiture_crees = 0
+    def __init__(self, marque, vitesse, prix):
         self.marque = marque
+        self.vitesse = vitesse
+        self.prix = prix
 
+    def __str__(self):
+        return f"Voiture de marque {self.marque} avec vitesse maximale de {self.vitesse}."
 
-voiture = Voiture("Ferrari")
+    @classmethod
+    def lamborghini(cls):
+        return cls(marque="Lamborghini", vitesse=250, prix=200000)
+
+    @classmethod
+    def porsche(cls):
+        return cls(marque="Porsche" ,vitesse=200, prix=180000)
+
+    # Pouvoir utiliser une méthode sans passer d'argument de classe
+    @staticmethod
+    def afficher_nombre_voitures():
+        print(f"Vous avez {Voiture.voiture_crees} voitures dans votre garage.")
+
+lambo = Voiture.lamborghini()
+porsche = Voiture.porsche()
+Voiture.afficher_nombre_voitures()
