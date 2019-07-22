70c73e36f68941f50398f9212a5b2ec0145bb47f
etienne <etienne@ubuntu-tinou.2le.net>
2019-07-22 14:24:16 +0200

    bdd JSON

---
+++ utils/bdd_json/app.py
@@ -0,0 +1,13 @@
+import json
+
+fichier = "settings.json"
+
+with open(fichier, 'r') as f:
+    settings = json.load(f)
+
+settings["fontSize"] = 15
+
+with open(fichier, 'w') as f:
+    json.dump(settings, f, indent=4)
+
+print(settings.get('fontSize'))
